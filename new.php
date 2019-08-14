afb9b2f3cd7cdcdd5bfa61c6fb53b62bbf559a16
Joseph Yhu <xbhlx7@gmail.com>
2019-08-13 23:20:07 -0400

    Update

--- new.php
+++ new.php
@@ -4,16 +4,18 @@
       <div class="new-entry">
         <h2>New Entry</h2>
         <form>
-          <label for="title"> Title</label>
-          <input id="title" type="text" name="title"><br>
-          <label for="date">Date</label>
-          <input id="date" type="date" name="date"><br>
-          <label for="time-spent"> Time Spent</label>
-          <input id="time-spent" type="text" name="timeSpent"><br>
-          <label for="what-i-learned">What I Learned</label>
-          <textarea id="what-i-learned" rows="5" name="whatILearned"></textarea>
+          <label for="title">Title (required)</label>
+          <input id="title" type="text" name="title" required><br>
+          <label for="date">Date (required)</label>
+          <input id="date" type="date" name="date" required><br>
+          <label for="time-spent">Time Spent (required)</label>
+          <input id="time-spent" type="text" name="timeSpent" required><br>
+          <label for="what-i-learned">What I Learned (required)</label>
+          <textarea id="what-i-learned" rows="5" name="whatILearned" required></textarea>
           <label for="resources-to-remember">Resources to Remember</label>
           <textarea id="resources-to-remember" rows="5" name="ResourcesToRemember"></textarea>
+          <label for="tags">Tags (separate with commas)</label>
+          <textarea id="tags", rows="5", name="tags"></textarea>
           <input type="submit" value="Publish Entry" class="button">
           <a href="#" class="button button-secondary">Cancel</a>
         </form>
