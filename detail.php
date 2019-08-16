92653ab742d2c872710a7f34ef6e5a3b9c95d393
Joseph Yhu <xbhlx7@gmail.com>
2019-08-16 19:31:17 -0400

    Update

--- detail.php
+++ detail.php
@@ -1,32 +1,31 @@
 <?php
+require 'inc/functions.php';
+
 $pageTitle = "The best day I've ever had";
 
 include 'inc/header.php'; ?>
   <section>
     <div class="container">
       <div class="entry-list single">
-        <article>
-          <h1>The best day I’ve ever had</h1>
-          <time datetime="2016-01-31">January 31, 2016</time>
-          <div class="entry">
-            <h3>Time Spent: </h3>
-            <p>15 Hours</p>
-          </div>
-          <div class="entry">
-            <h3>What I Learned:</h3>
-            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc ut rhoncus felis, vel tincidunt neque.</p>
-            <p>Cras egestas ac ipsum in posuere. Fusce suscipit, libero id malesuada placerat, orci velit semper metus, quis pulvinar sem nunc vel augue. In ornare tempor metus, sit amet congue justo porta et. Etiam pretium, sapien non fermentum consequat, <a href="">dolor augue</a> gravida lacus, non accumsan. Vestibulum ut metus eleifend, malesuada nisl at, scelerisque sapien.</p>
-          </div>
-          <div class="entry">
-            <h3>Resources to Remember:</h3>
-            <ul>
-              <li><a href="">Lorem ipsum dolor sit amet</a></li>
-              <li><a href="">Cras accumsan cursus ante, non dapibus tempor</a></li>
-              <li>Nunc ut rhoncus felis, vel tincidunt neque</li>
-              <li><a href="">Ipsum dolor sit amet</a></li>
-            </ul>
-          </div>
-        </article>
+        <?php
+        foreach (get_entry_list() as $item) {
+            echo "<article>";
+            echo "<h1>" . $item['title'] . "</h1>";
+            echo "<time datetime='" . $item['date'] . "'>" . date('F d, Y', strtotime($item['date'])) . "</time>";
+            echo "<div class='entry'>";
+            echo "<h3>Time Spent: </h3>";
+            echo "<p>" . $item['time_spent'] . "</p>";
+            echo "</div>";
+            echo "<div class='entry'>";
+            echo "<h3>What I Learned:</h3>";
+            echo "<p>" . $item['learned'] . "</p>";
+            echo "<div class='entry'>";
+            echo "<h3>Resources to Remember:</h3";
+            echo "<p>" . $item['resources'] . "</p>";
+            echo "</div>";
+            echo "</article>";
+       }
+       ?>
       </div>
     </div>
     <div class="edit">
