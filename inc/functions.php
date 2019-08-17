79faf8d83f5c738d8a6c0d992bb6326fb356b509
Joseph Yhu <xbhlx7@gmail.com>
2019-08-17 05:36:17 -0400

    Update functions.php

--- inc/functions.php
+++ inc/functions.php
@@ -3,7 +3,7 @@ function get_entry_list() {
     include 'connection.php';
 
     try {
-        return $db->query('SELECT title, date FROM entries ORDER BY date DESC');
+        return $db->query('SELECT id, title, date FROM entries ORDER BY date DESC');
     } catch (Exception $e) {
         echo "Error:" . $e->getMessage() . "<br>";
         return array();
