b21ccc9a3eb17456f44155d2d8893aeec9bef7be
Joseph Yhu <xbhlx7@gmail.com>
2019-09-11 15:21:08 -0400

    Update connection.php

--- inc/connection.php
+++ inc/connection.php
@@ -4,5 +4,5 @@ try {
     $db = new PDO("sqlite:".__DIR__."/journal.db");
     $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 } catch (Exception $e) {
-    echo $e->getMesssage();
+    echo $e->getMessage();
 }
