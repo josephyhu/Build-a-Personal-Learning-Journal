1c0fb692fb66d83367595f72b23ad1208a3b28fc
Joseph Yhu <xbhlx7@gmail.com>
2019-10-18 00:32:28 -0400

    Update

--- tags.php
+++ tags.php
@@ -7,7 +7,7 @@ $section = $tag;
 
 include 'inc/header.php'; ?>
   <section>
-    <h2><?php echo "#" . $tag; ?></h2>
+    <h2><?php echo "#" . trim($tag); ?></h2>
     <div class="container">
       <div class="entry-list">
         <?php
