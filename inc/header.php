ba5ce78026a72564f629f49a2d05f23213759a86
Joseph Yhu <xbhlx7@gmail.com>
2019-10-18 02:08:07 -0400

    Update header.php

--- inc/header.php
+++ inc/header.php
@@ -14,7 +14,7 @@
     <header>
       <div class="container">
         <div class="site-header">
-          <a class="logo" href="index.php"><i class="material-icons">library_books</i><br><p>Home</p></a>
+          <a class="logo" href="index.php"><i class="material-icons">library_books</i></a>
           <a class="button icon-right" href="new.php"><span>New Entry</span> <i class="material-icons">add</i></a>
         </div>
       </div>
