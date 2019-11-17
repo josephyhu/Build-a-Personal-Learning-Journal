f09386d11b1be18a181939843ad059e0a02c064f
Joseph Yhu <xbhlx7@gmail.com>
2019-11-17 04:34:06 -0500

    Delete

--- inc/footer_l.php
+++
@@ -1,9 +0,0 @@
-    <footer>
-      <div>
-        <a href="#top">^</a>
-        &copy; <?php echo date("Y"); ?> <a href="../about_l.php">Joseph Yhu</a>
-        <a href="#top">^</a>
-      </div>
-    </footer>
-  </body>
-</html>
